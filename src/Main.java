7a95eba0d362aecf3328f46e5ccb19cc0715c1a0
Gen <jdk094602@gmail.com>
2022-11-11 21:13:55 +0300

    l2

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+    char teest = 55;
+
+        int    varInt    = 1;
+        byte   varByte   = 2;
+        short  varShort  = 3;
+        long   varLong   = 4;
+        float  varFloat  = (float) 5.6;
+        double varDouble = 7.8;
+
+    System.out.println(
+            "Значение переменной " + "varInt" + " с типом" + "Int" + " равно " + varInt +  "\n"
+            + "Значение переменной " +  " с типом " +  " равно " + "\n"
+            + "Значение переменной " +  " с типом " +  " равно " + "\n"
+            + "Значение переменной " +  " с типом " +  " равно " + "\n"
+            + "Значение переменной " +  " с типом " +  " равно " + "\n"
+            + "Значение переменной " +  " с типом " +  " равно " + "\n"
+    );
+
+    }
+}
